e2bd08dbc21b729aaadcb69d2ea4bf4e8bf64378
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-08-19 15:02:04 -0400

    manager questions

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+//NPM needed for the app
 const inquirer = require('inquirer');
 
 
 function startApp() {
-    console.log("Welcome to the 'Build your team' app!");
+    console.log(
+`==================================================
+        Welcome to the 'Build your team' app!
+===================================================
+     `);
 
     function createManager() {
 
@@ -17,8 +22,43 @@ function startApp() {
                     }
                     return "Please enter the name.";
                 }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the Manager's ID number?",
+                validate: managerId => {
+                    if (managerId) {
+                        return true;
+                    }
+                    return "Please enter an ID number";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the Manager's e-mail address?",
+                validate: managerEmail => {
+                    if (managerEmail) {
+                        return true;
+                    }
+                    return "Please enter an ID number";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What is the Manager's office Number?",
+                validate: managerEmail => {
+                    if (managerEmail) {
+                        return true;
+                    }
+                    return "Please enter an office number";
+                }
             }
-        ])
+        ]).then(answers => {
+            console.log(answers);
+        })
     }
 
     createManager()
